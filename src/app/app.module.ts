2955c66706b688eb772af3263e606c8127335880
leninfer92 <lenin.chavez92@hotmail.com>
2023-07-20 13:46:17 -0500

    Cambios en el componente kilometraje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { routing } from './app.routing';
 
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavegadorComponent } from './components/navegador/navegador.component';
