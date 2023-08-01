5ce4b1ff40836e0b93be4704df3f3f1fa19e375c
leninfer92 <lenin.chavez92@hotmail.com>
2023-08-01 17:26:15 -0500

    Numeros negativos fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'biocity-app';
+
+  editingCarInfo: any ={anio: ''};
+
+  @HostListener('input', ['$event'])
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const sanitizedValue = input.value.replace(/[^0-9]/g, ''); // Eliminar caracteres no numéricos
+    if (parseInt(sanitizedValue) < 0) {
+      input.value = ''; // Si es negativo, establecer el valor como vacío
+    } else {
+      input.value = sanitizedValue; // Establecer el valor sin caracteres no numéricos
+    }}
 }
